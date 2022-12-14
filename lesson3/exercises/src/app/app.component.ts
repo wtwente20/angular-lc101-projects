8691e6102d4a7c36a02cd6bde4e49338cf883f0c
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-12-13 19:58:24 -0600

    saving progress for the night

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,33 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+    }
+   }
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+       let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+       rocketImage.style.left = movement;
+       this.width = this.width + 10000;
+    } else if (direction === 'left') {
+       let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+       rocketImage.style.left = movement;
+       this.width = this.width + 10000;
+    } else if (direction === 'up') {
+       let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+       rocketImage.style.bottom = movement;
+       this.width = this.width + 10000;
+    } else if (direction === 'bottom') {
+       let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+       rocketImage.style.bottom = movement;
+       this.width = this.width + 10000;
+    }
+   }
 }
